bef2a4c1f7878192fe0cd25646db8340be1e554b
Jackson S. Carneiro <jk25carneiro@gmail.com>
2024-12-18 23:13:32 -0300

    Adicionado em comprimento um ttítulo e as conversões para quilometro e milimetro

--- comprimento.c
+++ comprimento.c
@@ -1,10 +1,17 @@
 #include <stdio.h>
 
 int main(){
-    int comprimento;
+    int comprimento, kilometro, milimetro;
 
-    printf("Digite o comprimento em metros: ");
+    printf("\n\t----CONVERSAO DE MEDIDAS----\n");
+    printf("Digite o comprimento em metros: \n");
     scanf("%d", &comprimento);
+
+kilometro= comprimento/1000;
+milimetro = comprimento*1000;
+
+printf("O comprimento: %d \n Equivale a: %d quilometros!! \n" ,comprimento,kilometro);
+printf("O comprimento: %d \n Equivale a: %d milimetros!! \n" ,comprimento,milimetro);
 
     system("pause");
     return 0;
