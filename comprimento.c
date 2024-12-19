5e9a0a6b3600618ba9ea535633c8fb01f9b4f9f4
Fredson Santos <fredson.santos@outlook.com.br>
2024-12-19 00:28:03 -0300

    melhorado a formatação da saida printf de Kilometro L14

--- comprimento.c
+++ comprimento.c
@@ -1,16 +1,17 @@
 #include <stdio.h>
 
 int main(){
-    int comprimento, kilometro, milimetro;
+    int comprimento, milimetro;
+    float kilometro;
 
     printf("\n\t----CONVERSAO DE MEDIDAS----\n");
     printf("Digite o comprimento em metros: \n");
     scanf("%d", &comprimento);
 
-kilometro= comprimento/1000;
-milimetro = comprimento*1000;
+kilometro= comprimento/1000.0;
+milimetro = comprimento*1000.0;
 
-printf("O comprimento: %d \n Equivale a: %d quilometros!! \n" ,comprimento,kilometro);
+printf("O comprimento: %d \n Equivale a: %.2f quilometros!! \n" ,comprimento,kilometro); //adicionado %.2f para limitar a 2 casas decimais e float para kilometro
 printf("O comprimento: %d \n Equivale a: %d milimetros!! \n" ,comprimento,milimetro);
 
     system("pause");
